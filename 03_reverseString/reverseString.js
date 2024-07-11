39f1b534a1f79f34db45fc2a1715610cd66c4e5f
OmegaGinga <abdielflores980401@hotmail.com>
2024-07-11 17:47:01 -0600

    Test 1 passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(sentence) {
+
+    let splittedString = sentence.split(" ");
+    let reversedString = [];
+
+    splittedString.reverse();
+    for(word of splittedString){
+        reversedString.push(word.split("").reverse().join(""));
+    }
+
+    let joinedWord = reversedString.join(" ");
+    return joinedWord;
 
 };
-
 // Do not edit below this line
 module.exports = reverseString;
