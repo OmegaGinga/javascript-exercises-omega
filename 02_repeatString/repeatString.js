589b37bc992d0e2ccaba92bd6bf447d210bde05f
OmegaGinga <abdielflores980401@hotmail.com>
2024-07-11 17:09:30 -0600

    Test 5 passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
 const repeatString = function(text, times) {
     let outPut = "";
 
+    if(times < 0){
+        return "ERROR";
+    }
     for(let i = 1; i <= times; i++){
         outPut += text;
     }
