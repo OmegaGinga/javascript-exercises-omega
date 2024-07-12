bbd0805001c88bcee8cc0de8f771c68646f7da47
OmegaGinga <abdielflores980401@hotmail.com>
2024-07-11 18:27:08 -0600

    Test 2 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function(array, valueToRemove) {
-    array.splice(array.indexOf(valueToRemove), 1);
+const removeFromArray = function(array, value, value2, value3, value4) {
+    for (let i = array.length - 1; i >= 0; i--) {
+        if (array[i] === value || array[i] === value2 || array[i] === value3 || array[i] === value4) {
+            array.splice(i, 1);
+        }
+    }
 
     return array;
 };
