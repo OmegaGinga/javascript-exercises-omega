7915e35471f16220b11297a1443c884796da5632
OmegaGinga <abdielflores980401@hotmail.com>
2024-07-11 19:28:39 -0600

    Test 4 passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,13 +1,13 @@
 const leapYears = function(year) {
-
-    if(year% 4 === 0){
-        
+    if (year%4 === 0 && year%100 != 0 || year%400 ===0){
         return true;
-    }else{
+    }else {
         return false;
     }
 
 };
+
+console.log(leapYears(1600))
 
 // Do not edit below this line
 module.exports = leapYears;
