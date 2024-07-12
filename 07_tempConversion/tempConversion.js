f89c8882de0dc860775cf8993e97e4d05d523dc8
OmegaGinga <abdielflores980401@hotmail.com>
2024-07-11 19:40:01 -0600

    First test passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(celcius) {
+  let conversion = celcius - 32;
+  return conversion;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fahrenheit) {
+  let conversion = fahrenheit + 32;
+  return conversion;
 };
 
 // Do not edit below this line
