1365b553b3f5f13369b9f65dade37a2bbf8252ea
OmegaGinga <abdielflores980401@hotmail.com>
2024-07-11 19:00:34 -0600

    Test 5 passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const sumAll = function(number1, number2) {
 
     let result = 0;
 
-    if (number1 < 0 || number2 < 0){
+    if (number1 < 0 || number2 < 0 || isString(number1) || isString(number2)){
         return "ERROR";
 
     }
@@ -22,6 +22,10 @@ const sumAll = function(number1, number2) {
 
 };
 
-console.log(sumAll(123,-1));
+function isString(value){
+    return typeof value === 'string';
+}
+
+console.log(sumAll(1,12));
 // Do not edit below this line
 module.exports = sumAll;
