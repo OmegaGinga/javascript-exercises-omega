cfe8a85e70b692f2318ddbef235683b44a7207a0
OmegaGinga <abdielflores980401@hotmail.com>
2024-07-11 19:55:54 -0600

    Test 2 passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,21 @@
-const convertToCelsius = function(celcius) {
-  let conversion = celcius - 32;
-  return conversion;
+const convertToCelsius = function(celsius) {
+  let conversion = (celsius -32) / 1.8;  
+  let rounded = round(conversion,1);
+  return rounded;
 };
 
 const convertToFahrenheit = function(fahrenheit) {
-  let conversion = fahrenheit + 32;
-  return conversion;
+  
+
+  let conversion = (fahrenheit * 1.8) +32;
+  let rounded = round(conversion,1);
+  return rounded;
 };
+
+function round(value, presicion){
+  let multiplier = Math.pow(10, presicion || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
