30747112a11b02edf9d2a3a1f755a2a78d6259f2
OmegaGinga <abdielflores980401@hotmail.com>
2024-07-11 18:42:35 -0600

    first test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+
+    let result = 0;
+
+    for(let i = number1; i <= number2; i++){
+
+        result += i;
+
+    }    
+    return result;
 
 };
 
+console.log(sumAll(1,4));
 // Do not edit below this line
 module.exports = sumAll;
