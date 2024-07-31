2b844b16332344ad8039369c38e419611ad2a6b8
OmegaGinga <abdielflores980401@hotmail.com>
2024-07-31 10:38:12 -0600

    09 test 1 passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+
+    let new_word = '';
+    for(let i = word.length-1; i>=0;i--){
+        new_word+=word[i];
+    }
+
+    if(new_word === word){
+        return true;
+    }else{
+        return false;
+    }
 
 };
 
